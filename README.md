f7076327311b0c3bec66d8b67de213833bb6bc0c
Yann Lecommandoux <yann@requestlab.fr>
2018-08-20 18:48:12 +0200

    Add default method on MakeDomain + Update Readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 The RequestLab MakerBundle
 ==========================
 
-This bundle adds commands to generate code common to several of our projects.
+This bundle adds commands to generate code common to several of our projects.  It extends from the Symfony Maker Bundle
--- src/Maker/MakeDomain.php
+++ src/Maker/MakeDomain.php
@@ -2,12 +2,37 @@
 
 namespace RLB\Bundle\MakerBundle\Maker;
 
+use Symfony\Bundle\MakerBundle\ConsoleStyle;
+use Symfony\Bundle\MakerBundle\DependencyBuilder;
+use Symfony\Bundle\MakerBundle\Generator;
+use Symfony\Bundle\MakerBundle\InputConfiguration;
 use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 
+/**
+ * Class MakeDomain.
+ */
 final class MakeDomain extends AbstractMaker
 {
     public static function getCommandName() : string
     {
         return 'rlb:make:domain';
     }
+
+    public function configureCommand(Command $command, InputConfiguration $inputConfig)
+    {
+        // TODO: Implement configureCommand() method.
+    }
+
+    public function configureDependencies(DependencyBuilder $dependencies)
+    {
+        // TODO: Implement configureDependencies() method.
+    }
+
+    public function generate(InputInterface $input, ConsoleStyle $io, Generator $generator)
+    {
+        // TODO: Implement generate() method.
+    }
+
 }
